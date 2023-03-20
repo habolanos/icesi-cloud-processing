33e6774a69a7fc8aa1760a3b97ea02a084c4d6ab
Harold Adrian <haroldb@mill5.com>
2023-03-20 15:26:27 -0500

    Remove private key

--- terraform/sandbox-aws-ec2/provider_aws.tf
+++ terraform/sandbox-aws-ec2/provider_aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIARJTRNRZSLR6KYM47"
-  secret_key = "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"
+  access_key = "XXXXX"
+  secret_key = "zzzz"
 }
